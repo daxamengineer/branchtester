84a40c55d867f9c0a53b30eb4e176817831580aa
Parth Chauhan <parthchauhan03@gmail.com>
2024-08-17 10:57:24 +0530

    Compiled Files

--- Calc.py
+++ Calc.py
@@ -1,5 +1,30 @@
+import add
+import Subtraction
+import multiplication
+import Division
+
 print("Calculator")
 print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
+print("5. Exit")
+
+
+while(1):
+    choice = int(input("Enter Choice of Operation: "))
+
+    num1 = int(input("Enter Num 1 - "))
+    num2 = int(input("Enter Num 2 - "))
+
+    if choice == 1:
+        add.add(num1, num2)
+    elif choice == 2:
+        Subtraction.Subtract(num1, num2)
+    elif choice == 3:
+        multiplication.multiply(num1, num2)
+    elif choice == 4:
+        Division.division(num1, num2)
+    elif choice == 5:
+        break
+
--- add.py
+++ add.py
@@ -1,9 +1,7 @@
-num1 = input("Enter First Number")
-num2 = input("Enter Second Number")
+
 def add(num1, num2):
     add = num1+num2
     return add
-add(num1,num2)
 
 
 
